eb92b7ac12a8ab9a653cd6ee7854efe9f4d31ded
Konstantin Vorobyev <konstantin.31.12@yandex.ru>
2022-11-25 02:18:22 +0300

    Final

--- MultyThread/Form1.Designer.cs
+++ MultyThread/Form1.Designer.cs
@@ -30,22 +30,28 @@
         {
             this.pbT0 = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.tbT0 = new System.Windows.Forms.TextBox();
             this.bT0Click = new System.Windows.Forms.Button();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.rtbLog = new System.Windows.Forms.RichTextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.tbT1 = new System.Windows.Forms.TextBox();
             this.bT1Click = new System.Windows.Forms.Button();
             this.pbT1 = new System.Windows.Forms.PictureBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.tbT2 = new System.Windows.Forms.TextBox();
             this.bT3Click = new System.Windows.Forms.Button();
             this.pbT2 = new System.Windows.Forms.PictureBox();
             this.bTAll = new System.Windows.Forms.Button();
+            this.pbStatusT0 = new System.Windows.Forms.PictureBox();
+            this.pbStatusT1 = new System.Windows.Forms.PictureBox();
+            this.pbStatusT2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbT0)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox4.SuspendLayout();
@@ -53,6 +59,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbT1)).BeginInit();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbT2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatusT0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatusT1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatusT2)).BeginInit();
             this.SuspendLayout();
             // 
             // pbT0
@@ -66,6 +75,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.pbStatusT0);
+            this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.tbT0);
             this.groupBox1.Controls.Add(this.bT0Click);
@@ -77,22 +88,32 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thead 0";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Status: ";
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(131, 23);
+            this.label1.Location = new System.Drawing.Point(178, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.Size = new System.Drawing.Size(39, 15);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Delay";
+            this.label1.Text = "Delay:";
             // 
             // tbT0
             // 
-            this.tbT0.Location = new System.Drawing.Point(175, 19);
+            this.tbT0.Location = new System.Drawing.Point(220, 19);
             this.tbT0.Name = "tbT0";
-            this.tbT0.Size = new System.Drawing.Size(85, 23);
+            this.tbT0.Size = new System.Drawing.Size(40, 23);
             this.tbT0.TabIndex = 2;
             this.tbT0.Text = "100";
+            this.tbT0.TextChanged += new System.EventHandler(this.tbT0_TextChanged);
             // 
             // bT0Click
             // 
@@ -102,6 +123,7 @@
             this.bT0Click.TabIndex = 2;
             this.bT0Click.Text = "Start/Stop";
             this.bT0Click.UseVisualStyleBackColor = true;
+            this.bT0Click.Click += new System.EventHandler(this.bT0Click_Click);
             // 
             // groupBox4
             // 
@@ -123,6 +145,8 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.pbStatusT1);
+            this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.tbT1);
             this.groupBox2.Controls.Add(this.bT1Click);
@@ -134,22 +158,32 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Thead 1";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Status: ";
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(131, 23);
+            this.label2.Location = new System.Drawing.Point(178, 22);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.Size = new System.Drawing.Size(39, 15);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Delay";
+            this.label2.Text = "Delay:";
             // 
             // tbT1
             // 
-            this.tbT1.Location = new System.Drawing.Point(175, 19);
+            this.tbT1.Location = new System.Drawing.Point(220, 19);
             this.tbT1.Name = "tbT1";
-            this.tbT1.Size = new System.Drawing.Size(85, 23);
+            this.tbT1.Size = new System.Drawing.Size(40, 23);
             this.tbT1.TabIndex = 2;
             this.tbT1.Text = "200";
+            this.tbT1.TextChanged += new System.EventHandler(this.tbT1_TextChanged);
             // 
             // bT1Click
             // 
@@ -159,6 +193,7 @@
             this.bT1Click.TabIndex = 2;
             this.bT1Click.Text = "Start/Stop";
             this.bT1Click.UseVisualStyleBackColor = true;
+            this.bT1Click.Click += new System.EventHandler(this.bT1Click_Click);
             // 
             // pbT1
             // 
@@ -171,6 +206,8 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.pbStatusT2);
+            this.groupBox3.Controls.Add(this.label6);
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Controls.Add(this.tbT2);
             this.groupBox3.Controls.Add(this.bT3Click);
@@ -182,22 +219,32 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Thead 2";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(85, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Status: ";
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(131, 23);
+            this.label3.Location = new System.Drawing.Point(179, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(36, 15);
+            this.label3.Size = new System.Drawing.Size(39, 15);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Delay";
+            this.label3.Text = "Delay:";
             // 
             // tbT2
             // 
-            this.tbT2.Location = new System.Drawing.Point(175, 19);
+            this.tbT2.Location = new System.Drawing.Point(221, 19);
             this.tbT2.Name = "tbT2";
-            this.tbT2.Size = new System.Drawing.Size(85, 23);
+            this.tbT2.Size = new System.Drawing.Size(40, 23);
             this.tbT2.TabIndex = 2;
             this.tbT2.Text = "300";
+            this.tbT2.TextChanged += new System.EventHandler(this.tbT2_TextChanged);
             // 
             // bT3Click
             // 
@@ -207,6 +254,7 @@
             this.bT3Click.TabIndex = 2;
             this.bT3Click.Text = "Start/Stop";
             this.bT3Click.UseVisualStyleBackColor = true;
+            this.bT3Click.Click += new System.EventHandler(this.bT3Click_Click);
             // 
             // pbT2
             // 
@@ -226,6 +274,33 @@
             this.bTAll.Text = "Start/Stop all";
             this.bTAll.UseVisualStyleBackColor = true;
             this.bTAll.Click += new System.EventHandler(this.bTAll_Click);
+            // 
+            // pbStatusT0
+            // 
+            this.pbStatusT0.BackColor = System.Drawing.Color.Red;
+            this.pbStatusT0.Location = new System.Drawing.Point(126, 23);
+            this.pbStatusT0.Name = "pbStatusT0";
+            this.pbStatusT0.Size = new System.Drawing.Size(16, 16);
+            this.pbStatusT0.TabIndex = 6;
+            this.pbStatusT0.TabStop = false;
+            // 
+            // pbStatusT1
+            // 
+            this.pbStatusT1.BackColor = System.Drawing.Color.Red;
+            this.pbStatusT1.Location = new System.Drawing.Point(126, 23);
+            this.pbStatusT1.Name = "pbStatusT1";
+            this.pbStatusT1.Size = new System.Drawing.Size(16, 16);
+            this.pbStatusT1.TabIndex = 7;
+            this.pbStatusT1.TabStop = false;
+            // 
+            // pbStatusT2
+            // 
+            this.pbStatusT2.BackColor = System.Drawing.Color.Red;
+            this.pbStatusT2.Location = new System.Drawing.Point(126, 23);
+            this.pbStatusT2.Name = "pbStatusT2";
+            this.pbStatusT2.Size = new System.Drawing.Size(16, 16);
+            this.pbStatusT2.TabIndex = 7;
+            this.pbStatusT2.TabStop = false;
             // 
             // Form1
             // 
@@ -249,6 +324,9 @@
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbT2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatusT0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatusT1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatusT2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -273,5 +351,11 @@
         private Button bT3Click;
         private PictureBox pbT2;
         private Button bTAll;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private PictureBox pbStatusT0;
+        private PictureBox pbStatusT1;
+        private PictureBox pbStatusT2;
     }
 }
--- MultyThread/Form1.cs
+++ MultyThread/Form1.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
 
 namespace MultyThread
 {
@@ -9,36 +10,77 @@ namespace MultyThread
         {
             InitializeComponent();
             rtbLog.Clear();
+            InitializationThreads();
         }
 
-        public Task[] tasks = new Task[3];
-        
+        private Task[] _tasks { get; set; } = new Task[3];
+        private bool _isWork { get; set; } = false;
+        private bool _isAlreadyStart { get; set; } = false;
+        private bool _isWorkT0 = false;
+        private bool _isWorkT1 = false;
+        private bool _isWorkT2 = false;
+
+        private int _delayT0;
+        private int _delayT1;
+        private int _delayT2;
+
+        private bool _isInit { get; set; } = false;
+
+
         private void bTAll_Click(object sender, EventArgs e)
+        {
+            if (!_isAlreadyStart)
+            {
+                foreach (var t in _tasks)
+                    t.Start();
+                _isAlreadyStart = true;
+            }
+
+            WorkStatusUpdate();
+        }
+
+        public void InitializationThreads()
         {
             pbT0.Image = null; pbT1.Image = null; pbT2.Image = null;
             //rtbLog.Clear();
-            tasks[0] = new Task(() => UpdateThread(0, pbT0, Color.Yellow, Convert.ToInt32(tbT0.Text))); //left параллельные отрисовки
-            tasks[1] = new Task(() => UpdateThread(1, pbT1, Color.Blue, Convert.ToInt32(tbT1.Text))); //right
-            tasks[2] = new Task(() => UpdateThread(2, pbT2, Color.Green, Convert.ToInt32(tbT2.Text))); //center
-            foreach (var t in tasks)
-                t.Start();
-            //Task.WaitAll(tasks);
+            try
+            {
+                _delayT0 = Convert.ToInt32(tbT0.Text);
+                _delayT1 = Convert.ToInt32(tbT1.Text);
+                _delayT2 = Convert.ToInt32(tbT2.Text);
+
+                _tasks[0] = new Task(() => UpdateThread(0, pbT0, Color.Yellow, ref _delayT0, ref _isWorkT0)); //left параллельные отрисовки
+                _tasks[1] = new Task(() => UpdateThread(1, pbT1, Color.Blue, ref _delayT1, ref _isWorkT1)); //right
+                _tasks[2] = new Task(() => UpdateThread(2, pbT2, Color.Green, ref _delayT2, ref _isWorkT2)); //center
+            }
+            catch(Exception e)
+            {
+                AddToLog(e.Message);
+                return;
+            }
         }
 
-        public void UpdateThread(int treadId, PictureBox threadPB, Color threadColor, int threadDelay)
+        public void UpdateThread(int treadId, PictureBox threadPB, Color threadColor, ref int threadDelay, ref bool isWork)
         { // Form и RichTextBox передаются только ради обновления формы в IF'е, но это не работает
             Bitmap bmp = new Bitmap(threadPB.Width, threadPB.Height); // чтобы можно было пиксели picturebox заполнять
             for (int y = 0; y < bmp.Height; y++) // проходимся по высоте
             {
                 for (int x = 0; x < bmp.Width; x++) // по ширине
                 {
+
+                    while (!isWork)
+                    {
+                        Thread.Sleep(100);
+                    } 
+
                     bmp.SetPixel(x, bmp.Height - y - 1, threadColor); // ставим пиксель с указанными параметрами
                     
                     if (x % threadPB.Width == 0) // это условие для заметного поэтапного заполнения picturebox и richtextbox
                     {
-                        //logRTB.Text += $"Поток {threadDelay}: установил цвет {threadColor.Name} в пиксель ({x}, {y})\threadDelay";
-                        //logRTB.Invoke(new AddRTextDelegate(RTextAdd), threadColor.Name, x, y, threadDelay); не надо
-                        SetNewImage(threadPB, bmp); // обновляем threadPB
+                        //lock (threadPB) //блокировка ресурса
+                        //{
+                            SetNewImage(threadPB, bmp); // обновляем threadPB
+                        //}
 
                         AddToLog($"Поток {treadId}: установил цвет {threadColor.Name} в {y} строку пикселей)"); ;
 
@@ -58,7 +100,7 @@ namespace MultyThread
             rtbLog.BeginInvoke(new UpdateRichTextBoxDelegate(UpdateRichTextBox), logValue);
         }
 
-        public void UpdateRichTextBox(string logValue)
+        private void UpdateRichTextBox(string logValue)
         {
             rtbLog.AppendText(logValue + Environment.NewLine);
             rtbLog.ScrollToCaret();
@@ -72,11 +114,90 @@ namespace MultyThread
             imageControl.BeginInvoke(new UpdatePictureBoxDelegate(UpdatePictureBox), new object[] { imageControl, imageValue });
         }
 
-        public void UpdatePictureBox(PictureBox imageControl, Image imageValue)
+        private void UpdatePictureBox(PictureBox imageControl, Image imageValue)
         {
             imageControl.Image = imageValue;
         }
 
-        
+        private void WorkStatusUpdate()
+        {
+            _isWork = !_isWork;
+            _isWorkT0 = _isWorkT1 = _isWorkT2 = _isWork;
+
+            if (_isWork)
+                AddToLog($"Работа всех потоков поставлена на паузу");
+            else
+                AddToLog($"Работа всех потоков остановлен");
+
+            UpdateStatusThread();
+        }
+
+        private void bT0Click_Click(object sender, EventArgs e)
+        {
+            ThreadStateToLog(0, _isWorkT0 = !_isWorkT0);
+        }
+
+        private void bT1Click_Click(object sender, EventArgs e)
+        {
+            ThreadStateToLog(1, _isWorkT1 = !_isWorkT1);
+        }
+
+        private void bT3Click_Click(object sender, EventArgs e)
+        {
+            ThreadStateToLog(2, _isWorkT2 = !_isWorkT2);
+        }
+
+        private void ThreadStateToLog(int id, bool state)
+        {
+            if (state)
+                AddToLog($"Поток {id} поставлен на паузу");
+            else
+                AddToLog($"Поток {id} возобновил работу");
+
+            UpdateStatusThread();
+        }
+
+        private void UpdateStatusThread()
+        {
+            pbStatusT0.BackColor = _isWorkT0 ? Color.Green : Color.Red;
+            pbStatusT1.BackColor = _isWorkT1 ? Color.Green : Color.Red;
+            pbStatusT2.BackColor = _isWorkT2 ? Color.Green : Color.Red;
+        }
+
+        private void UpdateDelay(TextBox tb, int id)
+        {
+            try
+            {
+                var value = Convert.ToInt32(tb.Text);
+
+                switch (id)
+                {
+                    case 0: _delayT0 = value; break;
+                    case 1: _delayT1 = value; break;
+                    case 2: _delayT2 = value; break;
+                }
+
+                AddToLog($"Потоку {id} установлено новое время обновления: {value} мс");
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void tbT0_TextChanged(object sender, EventArgs e)
+        {
+            UpdateDelay(tbT0, 0);
+        }
+
+        private void tbT1_TextChanged(object sender, EventArgs e)
+        {
+            UpdateDelay(tbT1, 1);
+        }
+
+        private void tbT2_TextChanged(object sender, EventArgs e)
+        {
+            UpdateDelay(tbT2, 2);
+        }
     }
 }
